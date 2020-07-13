15b5ac114a8067523b9a1b16a76cb6184f627f6a
bladeofgods <bladeofgods@163.com>
2020-07-13 13:53:57 +0800

    add observer

---
+++ lib/base_framework/navigator/custom_navigator_observer.dart
@@ -0,0 +1,35 @@
+/*
+* Author : LiJiqqi
+* Date : 2020/7/13
+* 观察路由进出状态并做记录
+* 用于应用被杀死后的页面恢复
+*/
+
+
+class CustomNavigatorObserver extends NavigatorObserver{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
